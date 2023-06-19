88409bbd464cba6cc2769fc5a66f6b208e5ddd23
javacaoyu <javacaoyu@163.com>
2023-06-20 02:10:10 +0800

    演示logging模块的使用，自行构建3个类，模拟调用逻辑

--- tmp/t1.py
+++ tmp/t1.py
@@ -1,5 +1,20 @@
 
+class Dog:
+    def __init__(self, name):
+        self.name = name
 
-def make_upper(data):
-    return str(data).lower()
+    def wangwang(self):
+        print(f"我是一条小狗，我叫：{self.name}，汪汪汪")
 
+    @staticmethod
+    def dance():
+        print(f"我是小狗，我会跳舞。。。")
+
+# class：模板
+# 对象： 基于模板创建的
+
+# class：规划好的流水线
+# 对象：流水线生成的产品
+
+# class：手机流水线.输出流水线信息
+# 对象：Iphone6
